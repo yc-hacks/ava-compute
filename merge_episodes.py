712c66c1166a31cec0b440db66f813633a74bfe3
Kevin Hou <kevin.ch.hou@gmail.com>
2019-11-23 11:39:15 -0800

    Merged

--- merge_episodes.py
+++ merge_episodes.py
@@ -1,28 +1,35 @@
 import glob
 import os
+import re
 
 import pandas as pd
 
 from chunk_transcripts import transcript_to_chunks
 
 
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
 def create_df(fileName, maxChunkSize):
     df = pd.read_csv(fileName, names=["UUID", "title", "summary", "published", "link", "transcription"])
     df['paragraphs'] = df['transcription'].apply(lambda x: transcript_to_chunks(x, maxChunkSize))
 
     # Test: drop columns
-    df = df.drop("UUID", axis=1)
-    df = df.drop("summary", axis=1)
-    df = df.drop("published", axis=1)
-    df = df.drop("link", axis=1)
-    df = df.drop("transcription", axis=1)
+    # df = df.drop("UUID", axis=1)
+    # df = df.drop("summary", axis=1)
+    # df = df.drop("published", axis=1)
+    # df = df.drop("link", axis=1)
+    # df = df.drop("transcription", axis=1)
 
     expodedDF = []
     for i, row in df.iterrows():
         for paragraph in row['paragraphs']:
-            expodedDF.append((row['title'], paragraph))
+            clean_paragraph = cleanhtml(paragraph)
+            expodedDF.append((row['title'], clean_paragraph, row['UUID'], cleanhtml(row['summary']), row['link']))
     
-    return pd.DataFrame(expodedDF, columns = ['title', 'paragraphs']) 
+    return pd.DataFrame(expodedDF, columns = ['title', 'paragraphs', 'uuid', 'summary', 'link']) 
 
 
 
---
+++ merge_podcasts.py
@@ -0,0 +1,28 @@
+import glob
+import os
+import re
+
+import pandas as pd
+
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+def create_df(fileName):
+    return pd.read_csv(fileName, names=["UUID", "title", "author", "description", "category", "image"])
+
+if __name__ == "__main__":
+    os.chdir("./final-output/podcasts")
+
+    OUTPUT_FILE = "../merged_podcasts.csv"
+
+    extension = 'csv'
+    all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+    # combine all files in the list
+    combined_csv = pd.concat([create_df(f) for f in all_filenames ])
+
+    # export to csv
+    combined_csv.to_csv(OUTPUT_FILE, index=False, encoding='utf-8-sig')
