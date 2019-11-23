3f36e67a1bbf011e80fa6cf53826e5db4e5ccbc3
Theodor Marcu <marcu.theodor@gmail.com>
2019-11-23 07:47:56 -0800

    Added transcript generator

--- generate_transcripts.py
+++ generate_transcripts.py
@@ -7,14 +7,15 @@ from __future__ import print_function
 import time
 import boto3
 import argparse
+import csv
+import json
 
 # Command-line arguments.
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", help="increase output verbosity",
                     action="store_true")
-parser.add_argument("-u", "--url", help="URL to Podcast RSS Feed. ")
 parser.add_argument("-s", "--podcast_src", help="Home directory for podcasts to be transcribed.")
-parser.add_argument("-n", "--num", help="How many podcasts in the home directory should be transcribed.")
+parser.add_argument("-n", "--num", help="How many podcasts in the home directory should be transcribed.", type=int)
 parser.add_argument("-i", "--doc", help="Podcast Episode CSV File.")
 args = parser.parse_args()
 
@@ -26,40 +27,58 @@ podSrc = args.podcast_src
 basePath = f"s3://ava-compute-storage/{podSrc}"
 # How many podcasts to transcribe.
 podcastCount = args.num
+# Resulting path.
+resultingPath = f"{podSrc}/transcripts/episodes.csv"
 
 # Transcription Engine
 transcribe = boto3.client('transcribe')
 # S3 for file storage
 S3 = boto3.client('s3')
 
+SOURCE_FILENAME = resultingPath
+BUCKET_NAME = 'ava-compute-storage'
+
 # Open Podcast File.
-with open(episode_file, "rt") as episodeFile:
+with open(episode_file, "rt") as episodeFile, open(resultingPath, "w") as resultFile:
     episodeReader = csv.reader(episodeFile)
+    resultWriter = csv.writer(resultFile)
     episodes = list(episodeReader)
     countEpisodes = 0
-    for e in episodes:
-        print(e)
-#         jobName = f"{podSrc}_episode_{i}"
-#         countEpisodes += 1
-# for i in range(0, podcastCount):
-#     print(f"Transcribing {args.podcast_src} Episode {i} of {podcastCount}")
-#     jobName = f"{podSrc}_episode_{i}"
-#     job_uri = f"{basePath}/"
-#
-#     transcribe.start_transcription_job(
-#         TranscriptionJobName=job_name,
-#         Media={'MediaFileUri': job_uri},
-#         MediaFormat='mp3',
-#         LanguageCode='en-US'
-#     )
-#     while True:
-#         status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
-#         if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
-#             print(status)
-#             with open('data.txt', 'w') as outfile:
-#                 json.dump(data, outfile)
-#             S3.upload_file(SOURCE_FILENAME, BUCKET_NAME, SOURCE_FILENAME)
-#             break
-#         print(f"Job {jobName} Not ready yet...")
-#         time.sleep(5)
-#     print(status)
+    for i, e in enumerate(episodes):
+        if i > podcastCount:
+            break
+        jobName = f"{podSrc}_episode_{i}"
+        episode_id = e[0]
+        countEpisodes += 1
+        print(f"Transcribing {args.podcast_src} Episode {i} of {podcastCount}")
+        jobName = f"{podSrc}_episode_{i}"
+        job_uri = f"{basePath}/{episode_id}_episode_{i}.mp3"
+        print(jobName)
+        print(job_uri)
+        # Save Translated Json
+        # with open('asrOutput.json') as f:
+        #     d = json.load(f)
+        #     translated = d['results']['transcripts'][0]['transcript']
+        transcribe.start_transcription_job(
+            TranscriptionJobName=job_name,
+            Media={'MediaFileUri': job_uri},
+            MediaFormat='mp3',
+            LanguageCode='en-US'
+        )
+        while True:
+            status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+            if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+                print(status)
+                break
+            print(f"Job {jobName} Not ready yet...")
+            time.sleep(5)
+        translated = status['results']['transcripts'][0]['transcript']
+        f.close()
+        # print(translated)
+        e.append(translated)
+        resultWriter.writerow(e)
+
+
+S3.upload_file(SOURCE_FILENAME, BUCKET_NAME, SOURCE_FILENAME)
+episodeFile.close()
+resultFile.close()
